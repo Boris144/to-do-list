96da9e135347bb32827800dd24e773b67eb6d3d9
Boris144 <borisvutchev2000@gmail.com>
2022-06-25 10:48:27 +0300

    added delete function

--- logic.js
+++ logic.js
@@ -1,15 +1,24 @@
 const toDoList = document.getElementById('toDoList');
 const newItemInput = document.getElementById('newItemInput');
 
+function delte(event) {
+    event.target.parentElement.remove();
+}
+
 function toggleDone(event) {
     event.target.classList.toggle("done");
 }
 
 function add(){
-   const newItem = document.createElement('li');
-   newItem.addEventListener("click", toggleDone);
-   newItem.innerText = newItemInput.value;
-    toDoList.append(newItem);
+    const newItemButton = document.createElement('button');
+    newItemButton.innerText = 'X';
+    newItemButton.addEventListener("click", delte);
+
+    const newItem = document.createElement('li');
+    newItem.addEventListener("click", toggleDone);
+    newItem.innerText = newItemInput.value;
+    newItem.appendChild(newItemButton);
+    toDoList.append(newItem)
 
     newItemInput.value = '';
-}
+}https://github.com/Boris144/to-do-list.git
